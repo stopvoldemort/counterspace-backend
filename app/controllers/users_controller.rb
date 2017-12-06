8b92667a8be277401c36d6916b0c3588dab236be
stopvoldemort <david.squires1@gmail.com>
2017-12-06 18:56:22 -0500

    fixed create user issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,14 +8,8 @@ class UsersController < ApplicationController
     if user.save
       payload = {user_id: user.id}
       token = issue_token(payload)
-      current_user = {}
-      current_user[:user] = user
-      current_user[:reservations] = user.reservations
-      current_user[:kitchens] = user.kitchens
-      current_user[:kitchen_reviews] = user.reviewed_kitchens
-      current_user[:kitchen_pictures] = user.kitchens.map {|k| k.kitchen_pictures[0]}
-      current_user[:reviews_of_users_kitchens] = user.kitchens.map {|k| k.reviews}.flatten
-      render json: { jwt: token, current_user: current_user}
+      response = user.session_hash
+      render json: { jwt: token, response: response}
     else
       render json: { error: "There was an error with your account."}
     end
